01e9e8ada2d3f0b352ae395aa58f5ae49b052296
tonny <tonny@atuo.me>
2019-12-19 00:10:53 +0800

    upload tun util

--- src/nbtun.py
+++ src/nbtun.py
@@ -1,13 +1,22 @@
 import queue
 from threading import Thread
-# import pytun
+import pytun
 import json
+import subprocess
+import os
 
 class nbtun:
     def __init__(self, role="client"):
-        with open("../config/config.json", "r") as config_file:
-            self.config = json.loads( config_file.readlines() )
-        print(self.config)
+        self.callbacks = {}
+        with open("./config/config.json", "r") as config_file:
+            self.config = json.loads( "".join(config_file.readlines()) )
+        
+    
+    def get_config(self):
+        return self.config
+
+    def reg_callback(self, event: str, cb_func):
+        self.callbacks[event] = cb_func
 
 if __name__ == "__main__":
     tun = nbtun()
