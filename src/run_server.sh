309e9ac9cb912c8673443a61141dcb5ac77a1ab0
Gogo <gogo81745.moe@gmail.com>
2019-12-22 10:22:43 +0800

    update scripts

--- src/run_server.sh
+++ src/run_server.sh
@@ -8,6 +8,10 @@ echo "Author: Tonny-Gu & Gogo"
 echo "Version: Alpha.1211"
 echo "******************************"
 
+SCRIPT_DIR=$(dirname "$0")
+
+cd $SCRIPT_DIR
+
 PYTHON_EXEC=$(cat ../config/config.json | python -c "import sys, json; print(json.load(sys.stdin)['misc']['server']['py_exec_cmd'])")
 
 export PYTHONPATH=$PYTHONPATH:$(pwd)
